e81d20f585ff3f156a084788bbb8e3e1f2fb8bab
elixman <sylvain.barriere@icloud.com>
2017-06-07 17:01:31 +0200

    Minor updates

--- data.php
+++ data.php
@@ -1,10 +1,7 @@
-<? php
 
-//Connecting to sql db.
-$connect = mysqli_connect("my host","my user","my passwrod","my db");
-//Sending form data to sql db.
-mysqli_query($connect,"INSERT INTO posts (category, title, contents, tags)
-VALUES ('$_POST[post_category]', '$_POST[post_title]', '$_POST[post_contents]', '$_POST[post_tags]')";
+<?php
+$dbh = new PDO('mysql:host=localhost;dbname=wikicoda-pizza', $root, $jedi);
+
 
 
 
@@ -20,15 +17,7 @@ $tel = $_POST['tel'];
 $address = $_POST['address'];
 
 
-
-
-
-$sql = "INSERT INTO Directory_Services
-        VALUES ('$name', '$tel', '$address', now(), '$pizza1', '$pizza2', '$pizza3', '$pizza4', '$pizza5', '$pizza6')";
-
-
-
-
-
+$sql = "INSERT INTO Directory_Services(Name, Phone, Address, Date, Pizza1, Pizza2, Pizza3, Pizza4, Pizza5, Pizza6)
+        VALUES ($name, $tel, $address, now(), $pizza1, $pizza2, $pizza3, $pizza4, $pizza5, $pizza6)";
 
  ?>
